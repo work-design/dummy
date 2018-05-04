f8989879efc1abad81048b413652081747ae1d02
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-04 11:33:18 +0800

    test factory bot

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,10 +1,3 @@
-# Be sure to restart your server when you modify this file.
-#
-# This file contains migration options to ease your Rails 5.0 upgrade.
-#
-# Read the Rails 5.0 release notes for more info on each option.
-
-# Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = true
 
 # Enable origin-checking CSRF mitigation. Previous versions had false.
@@ -19,3 +12,7 @@ Rails.application.config.active_record.belongs_to_required_by_default = true
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+
+if defined?(FactoryBot)
+  FactoryBot.definition_file_paths << RailsLog::Engine.root.join('test/factories')
+end
