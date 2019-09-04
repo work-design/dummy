e52267623e4db8fdb3b31a081fdb8ce72f383b9f
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-04 15:26:20 +0800

    fix factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,8 +2,6 @@ FactoryBot.define do
   factory :user do
     id { 1 }
     name { "MyString" }
-    email { "MyString" }
-    mobile { "MyString" }
     password_digest { "MyString" }
     created_at { "2018-12-18 16:21:34" }
     updated_at { "2018-12-18 16:21:34" }
