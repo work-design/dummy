70534b3ddc91868d33849651683f5fd9191e6070
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-06 19:22:17 +0800

    default controoler concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  include RailsCom::Application
+  include RailsAuth::Application
+  
   protect_from_forgery with: :exception
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
+  
   root to: 'home#index'
-  #mount RailsCom::Engine, at: '/'
 
 end
