49a93a0955a8d5bd15e45be16a34536f57924240
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-06 19:49:23 +0800

    default form build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   include RailsCom::Application
   include RailsAuth::Application
   
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, unless: -> { json_format? }
+  default_form_builder 'ApplicationFormBuilder'
 end
