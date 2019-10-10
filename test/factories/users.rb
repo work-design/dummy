047c85346fbfb81dd5cfbe806e98eacc9f25011d
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-10 14:37:52 +0800

    default user factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
+  
   factory :user do
-    id { 1 }
     name { "MyString" }
-    password_digest { "MyString" }
-    created_at { "2018-12-18 16:21:34" }
-    updated_at { "2018-12-18 16:21:34" }
+    password { "111111" }
   end
+  
 end
