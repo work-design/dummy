ae1ccfcf1d3c078fe898c6a8d104f9bfba73c73a
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-26 10:02:00 +0800

    check if defined? rails auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include RailsCom::Application
-  include RailsAuth::Application
+  include RailsAuth::Application if defined? RailsAuth
   
   protect_from_forgery with: :exception, unless: -> { json_format? }
   default_form_builder 'ApplicationFormBuilder'
