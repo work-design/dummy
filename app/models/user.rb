fae3b5dfd56e3efec70aaca2b1076eb0e52176ff
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-29 11:52:44 +0800

    add more defautl

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   include RailsAuth::User if defined? RailsAuth
+  include RailsNotice::Receiver if defined? RailsNotice
+  include RailsOrg::User if defined? RailsOrg
 end
