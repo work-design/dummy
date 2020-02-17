e6c2eeed50391574ad33e4a4ff5ec6e7ddaa8ab9
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-17 15:34:47 +0800

    fix application

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,8 +1,8 @@
+import 'fomantic'
+
 import 'rails_com/application'
 import 'rails_com/stimulus'
 import 'default_form/application'
-
 import 'jquery'
 import 'moment'
-
 import '@work-design/ui/public/packs/js/semantic'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
-import 'rails_com/stimulus'
-import 'rails_com/application'
-import 'default_form/application'
+require.context('../images', true)
+import 'fomantic'
 
+import 'rails_com/application'
+import 'rails_com/stimulus'
+import 'default_form/application'
 import 'jquery'
 import 'moment'
-
 import '@work-design/ui/public/packs/js/semantic'
