4d2994853706fa04387608c422a6073567410fe4
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-19 20:35:23 +0800

    setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,53 +1,42 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
   config.eager_load = false
-
-  # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts += [
+    '.lvh.me'
+  ]
+
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
+    config.action_controller.enable_fragment_cache_logging = true
+    config.cache_store = :file_store, 'tmp/cache/store'
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailbox.ingress = :mailgun
 
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
 
   config.active_storage.service = :local
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.active_support.deprecation = :log
 end
+
 Rails.application.routes.default_url_options = {
-  host: 'localhost:3000'
+  protocol: 'http',
+  host: 'lvh.me',
+  port: 3000
 }
 ENV['EDITOR'] = 'code --wait'
