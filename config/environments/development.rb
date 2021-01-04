ecf18aa682ed20ed88214f50617b6c1b4519d616
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-04 20:56:05 +0800

    for test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.hosts += [
     '.lvh.me'
