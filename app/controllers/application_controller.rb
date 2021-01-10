f663966c3f710bd05cf4ead542578a1b03886a7d
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-10 12:09:30 +0800

    support board

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include RailsCom::Application
-  include RailsAuth::Application if defined? RailsAuth
-  
+  include Auth::Controller::Application if defined? RailsAuth
+
   protect_from_forgery with: :exception, unless: -> { json_format? }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
+  namespace :board, defaults: { namespace: 'board', business: 'application' } do
+    root '/board/home#index'
+  end
+
   mount RailsAuth::Engine, at: '/'
 
 end
