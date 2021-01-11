7bf0dd6f2c4dcdddb485fbc8612e72e2de2c9216
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-11 16:05:51 +0800

    fix err

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,7 +1,4 @@
 class MyController < ApplicationController
-  if defined? RailsAuth
-    include RailsAuth::Application
-    before_action :require_login
-  end
+  before_action :require_login
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   include RailsNotice::Receiver if defined? RailsNotice
   include RailsOrg::User if defined? RailsOrg
 
-
-
 end
