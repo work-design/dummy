0ba64216aee903b706d226ce46698d72689e24b8
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-11 09:57:30 +0800

    not use assign user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   include Auth::Model::User if defined? RailsAuth
   include RailsNotice::Receiver if defined? RailsNotice
   include RailsOrg::User if defined? RailsOrg
+
+
+
 end
