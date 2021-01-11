db6f31ac6e997cf6bd7f65df5c574fac97b21461
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-11 18:05:43 +0800

    rename to auth mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  include Auth::Model::User if defined? RailsAuth
+  include AuthModel::User if defined? RailsAuth
   include RailsNotice::Receiver if defined? RailsNotice
   include RailsOrg::User if defined? RailsOrg
 
