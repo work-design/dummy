f7d5e3d0a82146edd3b43e99c0f59d384a4b7880
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-13 21:36:01 +0800

    application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include RailsCom::Application
   include Auth::Controller::Application if defined? RailsAuth
+  include Roled::Controller::Application if defined? RailsRole
 
   protect_from_forgery with: :exception, unless: -> { json_format? }
 end
