79226691d16ffdd3a9d710f01269b1aaa833d898
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-13 08:49:56 +0800

    moved includes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include RailsCom::Application
-  include Auth::Controller::Application if defined? RailsAuth
+  include AuthController::Application if defined? RailsAuth
 
   protect_from_forgery with: :exception, unless: -> { json_format? }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  include AuthModel::User if defined? RailsAuth
   include RailsNotice::Receiver if defined? RailsNotice
   include RailsOrg::User if defined? RailsOrg
 
