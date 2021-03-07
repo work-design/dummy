746a8cc0acb3329213b85c271a02e923a15f8541
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-07 20:29:34 +0800

    moved namespce app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
-  namespace :board, defaults: { namespace: 'board', business: 'application' } do
+  namespace :board, defaults: { namespace: 'board' } do
     root '/board/home#index'
   end
 
