540451775dc8db9359c27db7a07fde00d0f7cd5c
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-27 21:55:27 +0800

    add webpacker

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,4 +1,4 @@
-const { webpackConfig, devServer, config, merge } = require('@rails/webpacker')
+const { webpackConfig, config, merge } = require('@rails/webpacker')
 const { resolve } = require('path')
 const paths = require('rails_ui')
 const customConfig = {
@@ -6,15 +6,9 @@ const customConfig = {
     extensions: ['.scss']
   }
 }
-delete devServer['hmr']
-delete devServer['disable_host_check']
-delete devServer['use_local_ip']
-delete devServer['pretty']
-delete devServer['watch_options']
 
 webpackConfig.entry = Object.assign(paths(), webpackConfig.entry)
 webpackConfig.resolve.modules = webpackConfig.resolve.modules.concat(resolve('node_modules'))
-webpackConfig.devServer = devServer
 
 debugger
 
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,4 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.RAILS_ENV = process.env.RAILS_ENV || 'development'
 
 const webpackConfig = require('./base')
 
