8c1c480835a4a3c2eb1888a671411b06c2526b57
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-18 11:12:39 +0800

    moved spring

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,6 @@
 require_relative 'boot'
-%w(
-  active_record/railtie
-  active_storage/engine
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  action_mailbox/engine
-  action_text/engine
-  rails/test_unit/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
-
+require 'rails_com/all'
 Bundler.require(*Rails.groups)
-#require 'rails_wechat'
 
 module Dummy
   class Application < Rails::Application
