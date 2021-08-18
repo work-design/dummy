6ddf8dc46c4610de730c6d75505946377b465533
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-18 11:00:31 +0800

    use singe puma config file

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,40 @@
-dir = ENV['PWD']
+require 'pry'
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+dir = @config.options[:app].root.to_s
+
+#workers 1
+threads min_threads_count, max_threads_count
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+port ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { 'development' }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+
+#state_path "#{File.expand_path('tmp/sockets/puma.state', dir)}"
+#prune_bundler true
+#early_hints true
+#bind "unix://#{File.expand_path('tmp/sockets/puma.sock', dir)}"
+state_path 'tmp/sockets/puma.state'
+activate_control_app "unix://#{File.expand_path('tmp/sockets/pumactl.sock', dir)}"
+stdout_redirect(
+  "#{File.expand_path('log/puma.stdout.log', dir)}",
+  "#{File.expand_path('log/puma.stdout.log', dir)}",
+  true
+)
 
 before_fork do
   ActiveRecord::Base.connection_pool.disconnect!
 end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 
-state_path "#{File.expand_path('tmp/sockets/puma.state', dir)}"
-binding.pry
-environment ENV.fetch("RAILS_ENV") { "development" }
+on_restart do
+  puts "\n-----> On restart"
+  puts "env: #{@options[:environment]}"
+  puts "pidfile: #{@options[:pidfile]}"
+  puts "binds: #{@options[:binds]}"
+  puts "control_url: #{@options[:control_url]}"
+end
+
+plugin :tmp_restart
