b13466fd04f8a3620f25574653143541fd12ed68
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-01 17:52:37 +0800

    over active storage base controller

---
+++ app/controllers/active_storage/base_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Override controller in active storage library
+class ActiveStorage::BaseController < ActionController::Base
+  include ActiveStorage::Streaming
+
+  protect_from_forgery with: :exception
+
+  self.etag_with_template_digest = false
+
+  before_action do
+    ActiveStorage::Current.url_options = {
+      protocol: request.protocol,
+      host: request.host,
+      port: request.port
+    }
+    ActiveStorage::Current.url_options.merge! host: SETTING.host if defined? SETTING
+  end
+end
