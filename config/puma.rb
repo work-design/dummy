348c9e115eae79b36947948cd67d4c3fa8e98e6a
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-01 19:35:23 +0800

    migration

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-require 'pry'
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 dir = @config.options[:app].root.to_s
