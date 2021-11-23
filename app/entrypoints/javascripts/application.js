af10c6fd13549e47e8679e05557359fdab916c18
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-23 09:08:14 +0800

    entrypoints

--- app/entrypoints/javascripts/application.js
+++ app/entrypoints/javascripts/application.js
@@ -3,9 +3,7 @@ for (const path in modules) {
   console.log(path)  // 如果没有引用的话，无法编译进 manifest
 }
 
-// css
-import '../stylesheets/css.scss'
-import 'rails_design_engine_ui/stylesheets/app.scss'
-import '@fortawesome/fontawesome-free/css/all.css'
+import 'rails/stylesheets/css.scss' // css
 
 import 'rails_design_engine_ui/javascripts/rails_design'
+application.debug = true
