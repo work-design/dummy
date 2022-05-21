fc3efb76a5b934aeb6f660ecc4d06af2530d8df0
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-21 17:59:57 +0800

    if good job exist

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,6 @@ Rails.configuration.assets.paths += [
   Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts').to_s
 ]
 
-Rails.configuration.assets.excluded_paths << GoodJob::Engine.root.join('app/assets/vendor').to_s
+Rails.configuration.assets.excluded_paths << GoodJob::Engine.root.join('app/assets/vendor').to_s if defined? GoodJob
 Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts').to_s
 Rails.configuration.assets.excluded_paths << ActionView::Railtie.root.join('lib/assets/compiled')
