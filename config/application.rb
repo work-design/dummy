a9025019249599c6b14334fdcccb8718c3d44b6c
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-19 11:20:23 +0800

    udpat3 config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,12 @@ module Dummy
       '.one.work',
       '.work.design'
     ]
+
+    config.server_timing = true
+    config.middleware.delete ActionDispatch::RequestId # 记录X-Request-Id（方便查看请求在群集中的哪台执行）
+
+    config.active_record.encryption.support_unencrypted_data = true
+
   end
 end
 
