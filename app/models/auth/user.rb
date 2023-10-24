1b961fb4633d0bdbabaf71942ec3275e39087c6a
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-24 11:29:19 +0800

    fix error

--- app/models/auth/user.rb
+++ app/models/auth/user.rb
@@ -1,7 +1,7 @@
 module Auth
   class User < ActiveRecord::Base
     include Model::User if defined? RailsAuth
-    include Notice::Model::User if defined? RailsNotice
+    include Notice::Ext::User if defined? RailsNotice
     include Org::Ext::User if defined? RailsOrg
   end
 end
