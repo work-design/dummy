b40b5691e45171906a3ff20bcd34a2ad8ce13065
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-09 14:06:36 +0800

    fix error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   if defined? RailsCom
     include Com::Controller::ErrHandler
     include Com::Controller::Application
+    include Roled::Controller::Application
   end
   include Auth::Controller::Application if defined? RailsAuth
-  include Roled::Controller::Application if defined? RailsRole
   include Org::Controller::Application if defined? RailsOrg
 
   protect_from_forgery with: :exception, unless: -> { json_format? }
