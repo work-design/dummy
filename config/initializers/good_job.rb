67a13faa554fb96f06175a9190541cf4fc2c6fb2
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-06 22:03:25 +0800

    goodjob

--- config/initializers/good_job.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.configure do
-  config.good_job.preserve_job_records = true
-  config.good_job.logger = ActiveSupport::Logger.new('log/good_job.log')
-  config.good_job.retry_on_unhandled_error = false
-end
