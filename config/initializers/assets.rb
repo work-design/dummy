47835cda6c3ec2d850c168b30462ac61cfae01a8
qinmingyuan <mingyuan0715@foxmail.com>
2024-06-15 20:03:05 +0800

    fix assets configs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
 Rails.configuration.assets.paths += [
-  Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts').to_s
+  Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts'),
+  Rails.root.join('app/assets/images'),
+  RailsDesign::Engine.root.join('app/assets/images')
 ]
 
-Rails.configuration.assets.excluded_paths ||= []
-Rails.configuration.assets.excluded_paths << GoodJob::Engine.root.join('app/assets/vendor').to_s if defined? GoodJob
-Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts').to_s if defined? ActionText
-Rails.configuration.assets.excluded_paths << ActionView::Railtie.root.join('lib/assets/compiled') if ActionView::Railtie.respond_to?(:root)
+Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts') if defined? ActionText
+Rails.configuration.assets.excluded_paths << ActionView::Railtie.root.join('lib/assets/compiled') if defined? ActionView
